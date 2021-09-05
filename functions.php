c3230045d16f48e7786cd9ebd409550fb60df068
Shariflock <shariflocked@gmail.com>
2021-09-05 23:46:51 +0600

    Theme Bootstrapping
    add after theme supports

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php 
+
+function mark_theme_setup () {
+    load_theme_textdomain('mark', get_template_directory_uri()."/languages");
+    add_theme_support('post-thumbnails');
+    add_theme_support('title_tag');
+    add_theme_support('automatic-feed-links');
+    add_theme_support('html5', array(
+        'search-form',
+        'comment-form',
+        'gallery',
+        'caption',
+        'comment-list'
+    ));
+    add_theme_support('custom-logo');
+    add_theme_support("top-menu", __("Top Menu", "mark"));
+}
+
+add_action("after_setup_theme", "mark_theme_setup");
